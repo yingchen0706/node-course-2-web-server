00e6985477681f2bd2da030e9e975dfd880b77dc
yingchen0706 <yingchen0706@gmail.com>
2017-07-07 16:47:59 +0800

    init commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set(
+    'view engine', 'hbs'
+);
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log =`${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs');
+});
+
+app.get('/', (req, res) => {
+    res.render('welcome.hbs', {
+        username: 'Ying',
+        pageTitle: 'Welcome Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errMsg: 'bad request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up and listening port 3000');
+});
